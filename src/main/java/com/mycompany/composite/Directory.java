ec8bac9f2a562c5012a0ab18fd9ab2a768a0a6e7
Sebastian Hernandez <sdhernandez98@ucatolica.edu.co>
2024-09-14 21:45:57 -0500

    Agregamos la clase directorio.

---
+++ src/main/java/com/mycompany/composite/Directory.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.composite;
+
+/**
+ *
+ * @author XPG
+ */
+import java.util.ArrayList;
+import java.util.List;
+
+public class Directory implements FileSystemComponent {
+    private String name;
+    private List<FileSystemComponent> components = new ArrayList<>();
+
+    public Directory(String name) {
+        this.name = name;
+    }
+
+    public void addComponent(FileSystemComponent component) {
+        components.add(component);
+    }
+
+    public void removeComponent(FileSystemComponent component) {
+        components.remove(component);
+    }
+
+    @Override
+    public void showDetails() {
+        System.out.println("Directory: " + name);
+        for (FileSystemComponent component : components) {
+            component.showDetails();
+        }
+    }
+}
