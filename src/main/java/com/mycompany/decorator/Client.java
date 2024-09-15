676458b83cc98f6821a0819497d75f156e1ddde2
Sebastian Hernandez <sdhernandez98@ucatolica.edu.co>
2024-09-15 01:57:21 -0500

    Se finaliza el patron decorator.

---
+++ src/main/java/com/mycompany/decorator/Client.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.decorator;
+
+/**
+ *
+ * @author XPG
+ */
+public class Client {
+    public static void main(String[] args) {
+        // Crear un café básico
+        Coffee coffee = new BasicCoffee();
+        System.out.println(coffee.getDescription() + " costs $" + coffee.cost());
+
+        // Decorar el café con leche
+        Coffee milkCoffee = new MilkDecorator(coffee);
+        System.out.println(milkCoffee.getDescription() + " costs $" + milkCoffee.cost());
+
+        // Decorar el café con leche y azúcar
+        Coffee sugarMilkCoffee = new SugarDecorator(milkCoffee);
+        System.out.println(sugarMilkCoffee.getDescription() + " costs $" + sugarMilkCoffee.cost());
+    }
+}
