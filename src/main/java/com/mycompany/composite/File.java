113c924000b608e3b473e595a454191b0ff4ac71
Sebastian Hernandez <sdhernandez98@ucatolica.edu.co>
2024-09-14 21:43:08 -0500

    Empezamos el patron composite, creando la interfaz y la clase que pretende completarla

---
+++ src/main/java/com/mycompany/composite/File.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.composite;
+
+/**
+ *
+ * @author XPG
+ */
+public class File implements FileSystemComponent {
+    private String name;
+
+    public File(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void showDetails() {
+        System.out.println("File: " + name);
+    }
+}
