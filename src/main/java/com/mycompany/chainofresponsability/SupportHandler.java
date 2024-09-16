1274e3f20707c63632b3f3efd33a8c0d107f9aa9
Sebastian Hernandez <sdhernandez98@ucatolica.edu.co>
2024-09-15 22:18:06 -0500

    Empezamos el patron Chain of responsability, esta interfaz define el método que todos los manejadores (handlers) deben implementar, y también maneja la referencia al siguiente manejador en la cadena.

---
+++ src/main/java/com/mycompany/chainofresponsability/SupportHandler.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.chainofresponsability;
+
+/**
+ *
+ * @author XPG
+ */
+public abstract class SupportHandler {
+    protected SupportHandler nextHandler;
+
+    public void setNextHandler(SupportHandler nextHandler) {
+        this.nextHandler = nextHandler;
+    }
+
+    public abstract void handleRequest(String requestType);
+}
+
