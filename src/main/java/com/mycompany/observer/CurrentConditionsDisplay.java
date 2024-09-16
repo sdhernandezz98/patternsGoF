975c1790205099a747e266d3a9ad9ea8b26ba672
Sebastian Hernandez <sdhernandez98@ucatolica.edu.co>
2024-09-15 23:42:20 -0500

    Agregamos dos clases que complementan la clase Observer.

---
+++ src/main/java/com/mycompany/observer/CurrentConditionsDisplay.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.observer;
+
+/**
+ *
+ * @author XPG
+ */
+public class CurrentConditionsDisplay implements Observer {
+    private float temperature;
+    private float humidity;
+    private Subject weatherStation;
+
+    public CurrentConditionsDisplay(Subject weatherStation) {
+        this.weatherStation = weatherStation;
+        weatherStation.registerObserver(this);
+    }
+
+    @Override
+    public void update(float temperature, float humidity, float pressure) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        display();
+    }
+
+    public void display() {
+        System.out.println("Current conditions: " + temperature + "C degrees and " + humidity + "% humidity");
+    }
+}
---
+++ src/main/java/com/mycompany/observer/StatisticsDisplay.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.observer;
+
+/**
+ *
+ * @author XPG
+ */
+public class StatisticsDisplay implements Observer {
+    private float maxTemp = 0.0f;
+    private float minTemp = 200;
+    private float tempSum = 0.0f;
+    private int numReadings;
+    private Subject weatherStation;
+
+    public StatisticsDisplay(Subject weatherStation) {
+        this.weatherStation = weatherStation;
+        weatherStation.registerObserver(this);
+    }
+
+    @Override
+    public void update(float temperature, float humidity, float pressure) {
+        tempSum += temperature;
+        numReadings++;
+
+        if (temperature > maxTemp) {
+            maxTemp = temperature;
+        }
+
+        if (temperature < minTemp) {
+            minTemp = temperature;
+        }
+
+        display();
+    }
+
+    public void display() {
+        System.out.println("Prom/Max/Min temperatura = " + (tempSum / numReadings) + "/" + maxTemp + "/" + minTemp);
+    }
+}
