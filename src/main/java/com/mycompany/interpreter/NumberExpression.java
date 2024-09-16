5767a3950d7c3ac71291a8c39632522695242303
Sebastian Hernandez <sdhernandez98@ucatolica.edu.co>
2024-09-15 22:54:46 -0500

    Interfaz Expression: Define un método interpret() que las clases concretas deben implementar para evaluar la expresión.
    Clase NumberExpression:
    Representa un número y simplemente devuelve su valor al ser interpretado.

---
+++ src/main/java/com/mycompany/interpreter/NumberExpression.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.interpreter;
+
+/**
+ *
+ * @author XPG
+ */
+public class NumberExpression implements Expression {
+    private int number;
+
+    public NumberExpression(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public int interpret() {
+        return number;
+    }
+}
+
