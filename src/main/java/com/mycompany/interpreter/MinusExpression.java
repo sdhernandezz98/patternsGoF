a9b3838bfc70e9b2dba8c619ce0eabb51ba00952
Sebastian Hernandez <sdhernandez98@ucatolica.edu.co>
2024-09-15 22:56:31 -0500

    Representan las operaciones de suma y resta. Cada una toma dos expresiones (izquierda y derecha) y las evalúa según la operación matemática.

---
+++ src/main/java/com/mycompany/interpreter/MinusExpression.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.interpreter;
+
+/**
+ *
+ * @author XPG
+ */
+public class MinusExpression implements Expression {
+    private Expression leftExpression;
+    private Expression rightExpression;
+
+    public MinusExpression(Expression leftExpression, Expression rightExpression) {
+        this.leftExpression = leftExpression;
+        this.rightExpression = rightExpression;
+    }
+
+    @Override
+    public int interpret() {
+        return leftExpression.interpret() - rightExpression.interpret();
+    }
+}
+
---
+++ src/main/java/com/mycompany/interpreter/PlusExpression.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.interpreter;
+
+/**
+ *
+ * @author XPG
+ */
+public class PlusExpression implements Expression {
+    private Expression leftExpression;
+    private Expression rightExpression;
+
+    public PlusExpression(Expression leftExpression, Expression rightExpression) {
+        this.leftExpression = leftExpression;
+        this.rightExpression = rightExpression;
+    }
+
+    @Override
+    public int interpret() {
+        return leftExpression.interpret() + rightExpression.interpret();
+    }
+}
+
