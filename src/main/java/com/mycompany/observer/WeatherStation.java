62664e745b1fa19566b98a7d4cd8d748e28dd089
Sebastian Hernandez <sdhernandez98@ucatolica.edu.co>
2024-09-15 23:36:18 -0500

    Empezamos el patron observer, tomadno como ejemplo una estacion y como las pantallas mostraran el cambio reflejado en este.

---
+++ src/main/java/com/mycompany/observer/WeatherStation.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.observer;
+
+/**
+ *
+ * @author XPG
+ */
+import java.util.ArrayList;
+import java.util.List;
+
+public class WeatherStation implements Subject {
+    private List<Observer> observers;
+    private float temperature;
+    private float humidity;
+    private float pressure;
+
+    public WeatherStation() {
+        observers = new ArrayList<>();
+    }
+
+    @Override
+    public void registerObserver(Observer o) {
+        observers.add(o);
+    }
+
+    @Override
+    public void removeObserver(Observer o) {
+        observers.remove(o);
+    }
+
+    @Override
+    public void notifyObservers() {
+        for (Observer observer : observers) {
+            observer.update(temperature, humidity, pressure);
+        }
+    }
+
+    public void setMeasurements(float temperature, float humidity, float pressure) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.pressure = pressure;
+        measurementsChanged();
+    }
+
+    public void measurementsChanged() {
+        notifyObservers();
+    }
+}
+
