e7e90b27286a0c7daeb63a321871cc0107292e16
Sebastian Hernandez <sdhernandez98@ucatolica.edu.co>
2024-09-15 23:59:21 -0500

    Empezamos el patron strategy, creando la interfaz que usaremos como intermedio.

---
+++ src/main/java/com/mycompany/strategy/DiscountStrategy.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.strategy;
+
+/**
+ *
+ * @author XPG
+ */
+public interface DiscountStrategy {
+    double applyDiscount(double total);
+}
+
