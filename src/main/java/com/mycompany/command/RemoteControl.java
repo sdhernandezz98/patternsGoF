0a26da34427e8c6d1727e0fc1167bae0a9871efc
Sebastian Hernandez <sdhernandez98@ucatolica.edu.co>
2024-09-15 22:46:51 -0500

    El invocador mantiene una referencia a los comandos y ejecuta el comando cuando se presiona un botón.

---
+++ src/main/java/com/mycompany/command/RemoteControl.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.command;
+
+/**
+ *
+ * @author XPG
+ */
+public class RemoteControl {
+    private Command command;
+
+    public void setCommand(Command command) {
+        this.command = command;
+    }
+
+    public void pressButton() {
+        command.execute();
+    }
+
+    public void pressUndo() {
+        command.undo();
+    }
+}
+
