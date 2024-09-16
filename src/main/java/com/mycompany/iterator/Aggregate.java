23b36023ca846296577171735b164802ae4d2ea8
Sebastian Hernandez <sdhernandez98@ucatolica.edu.co>
2024-09-15 23:03:04 -0500

    Creamos dos interfaces que tienen como fin
    Define dos métodos: hasNext(), que verifica si hay más elementos en la colección, y next(), que devuelve el siguiente elemento.
    
    Define el método createIterator(), que las colecciones deben implementar para devolver un iterador.

---
+++ src/main/java/com/mycompany/iterator/Aggregate.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.iterator;
+
+/**
+ *
+ * @author XPG
+ */
+public interface Aggregate {
+    Iterator createIterator();
+}
+
---
+++ src/main/java/com/mycompany/iterator/Iterator.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.iterator;
+
+/**
+ *
+ * @author XPG
+ */
+public interface Iterator {
+    boolean hasNext();
+    Object next();
+}
+
