db1b2fd1bd54a1980911b65920d75ad916bb6e62
Sebastian Hernandez <sdhernandez98@ucatolica.edu.co>
2024-09-16 00:19:00 -0500

    Hacemos el ultimo patron relacionado al visitor, y creamos 2 clases y 2 interfaces para iniciar

---
+++ src/main/java/com/mycompany/visitor/Magazine.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.visitor;
+
+/**
+ *
+ * @author XPG
+ */
+public class Magazine implements Element {
+    private String title;
+    private double price;
+
+    public Magazine(String title, double price) {
+        this.title = title;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public void accept(Visitor visitor) {
+        visitor.visit(this);
+    }
+}
+
