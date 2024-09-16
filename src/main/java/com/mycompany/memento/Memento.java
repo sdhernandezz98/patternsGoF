094ef2ba5f241d9787d7fd8e5ba9511c94bdae75
Sebastian Hernandez <sdhernandez98@ucatolica.edu.co>
2024-09-15 23:25:26 -0500

    Vamos a simular una aplicación de edición de texto en la que puedes escribir texto y deshacer cambios usando el patrón Memento.

---
+++ src/main/java/com/mycompany/memento/Memento.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.memento;
+
+/**
+ *
+ * @author XPG
+ */
+public class Memento {
+    private final String state;
+
+    public Memento(String state) {
+        this.state = state;
+    }
+
+    public String getState() {
+        return state;
+    }
+}
+
